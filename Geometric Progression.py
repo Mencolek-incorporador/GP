cecbed82c5d99f1a25238550cf5b4c7ba636fdbe
Mencolek-incorporador <109420088+Mencolek-incorporador@users.noreply.github.com>
2022-09-13 21:32:13 +0530

    Add files via upload

---
+++ Geometric Progression.py
@@ -0,0 +1,11 @@
+#Program to create a G.P. using the common ratio and first term of the user's choice
+cr=int(input("enter common ratio:"))
+ft=int(input("enter first term:"))
+te=int(input("enter number of terms:"))
+te=te-2
+i=0
+print(ft)
+while i <=te:
+    ft*=cr
+    i+=1
+    print(ft)
